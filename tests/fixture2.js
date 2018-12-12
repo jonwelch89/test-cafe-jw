6e57d4a35c1e5b85255a2279d7954f5d76459964
Jonathan Welch <jonathanwelch@notonthehighstreet.com>
2018-12-12 17:18:35 +0000

    Multiple fixtures

--- tests/fixture2.js
+++ tests/fixture2.js
@@ -14,9 +14,19 @@ test('A google search', async browser => {
         .click(".aajZCb input[type=submit]:nth-child(1)")
         .wait(1000)
         .expect(Selector('#resultStats').exists).ok()
-        console.log("This test has successfully passed")
-;
-
-        
-     
+        .expect(getPageUrl()).contains('Arsenal&btnK=Google+Search&oq=Arsenal')
+        console.log("This test has successfully passed") 
 });
+
+test
+    .page `https://www.bing.com/`
+    ('Test2', async browser => {
+    await browser
+        .click('<>')
+        .wait(1000)
+        .typeText('<>' , "<>")
+        .pressKey('enter')
+        .wait(1000)
+        .expect(Selector('<>').exists).ok()
+
+    });
